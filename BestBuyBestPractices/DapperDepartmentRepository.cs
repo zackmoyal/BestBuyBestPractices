e251ac6c2885d80eccb2acade6f5c7928e4d6f2d
Zack Moyal <zack.moyal@gmail.com>
2024-07-23 09:10:16 -0700

    adding steps 1-12

---
+++ BestBuyBestPractices/DapperDepartmentRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+	public class DapperDepartmentRepository : IDepartmentRepository
+	{
+		private readonly IDbConnection _conn;
+
+		public DapperDepartmentRepository(IDbConnection conn)
+		{
+			_conn = conn;
+		}
+		public IEnumerable<Department> GetAllDepartments()
+		{
+			return _conn.Query<Department>("SELECT * FROM departments");
+		}
+
+		public void InsertDepartment(string name)
+		{
+			_conn.Execute("INSERT INTO departments (Name) VALUE (@name)",
+				new { name = name });
+		}
+	}
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Zack's New Department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+	Console.WriteLine(department.DepartmentID);
+	Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
